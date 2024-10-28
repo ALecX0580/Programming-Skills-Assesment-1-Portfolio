b0a61795b39c293cad5fbe0f2dcb73a0f70176c5
Alen Alinas <efraim.alinas@gmail.com>
2024-10-28 09:29:26 +0400

    Delete Exercise 02 - Simple Sums directory

--- Exercise 02 - Simple Sums/20241013_Fall24-Exercise 2.py
+++
@@ -1,9 +0,0 @@
-# Declare two variables and initialize them with the integer values 8 and 10
-first = 8
-second = 10
-
-# Declare a variable and peform arithmetic operation
-sum = first + second
-
-# Print result of sum
-print("The sum of", first, "and", second, "is:", sum) 
