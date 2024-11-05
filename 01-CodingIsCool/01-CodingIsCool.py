47e9017882ef8cf6bca3d7743ccb1d46a48cb00a
Alen Alinas <efraim.alinas@gmail.com>
2024-11-05 13:31:24 +0400

    Added Exercise 1
    With Documentation in Code

---
+++ 01-CodingIsCool/01-CodingIsCool.py
@@ -0,0 +1,10 @@
+# Joins strings together to make a phrase
+
+# Assign string to each variable using sentence "Coding is Cool"
+word1 = "Coding "   # First word of phrase
+word2 = "is "       # Second word of phrase
+word3 = "Cool"      # Third word of phrase
+
+# Use string concatenation to combine the variables and print the phrase
+# Concatenate word1, word2, and word3 to output "Coding is Cool"
+print(word1 + word2 + word3)
