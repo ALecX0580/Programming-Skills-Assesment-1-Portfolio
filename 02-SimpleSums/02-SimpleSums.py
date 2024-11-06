4ba6dd443c8102920048d93b8e8a55484c954416
Alen Alinas <efraim.alinas@gmail.com>
2024-11-06 09:31:42 +0400

    Added exercise 2 & 3
    With documentation in code

---
+++ 02-SimpleSums/02-SimpleSums.py
@@ -0,0 +1,9 @@
+# Declare two variables and initialize them with the integer values 8 and 10
+first = 8
+second = 10
+
+# Declare a variable and perform arithmetic operation
+sum = first + second
+
+# Print result of sum
+print("The sum of", first, "and", second, "is:", sum) 
